c91a9ea6d53cc09eb42eb46dbe617f0175a0d110
Juni <junikimdraws@gmail.com>
2025-06-24 19:58:45 -0500

    revert

--- styles/js/pages/characters.js
+++ styles/js/pages/characters.js
@@ -8,49 +8,32 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-  let dex = null;
-  let logs = null;
-  try {
-    dex = await charadex.initialize.page(
-      null,
-      charadex.page.masterlist,
-      null,
-      async (listData) => {
+  let dex = await charadex.initialize.page(
+    null,
+    charadex.page.masterlist,
+    null,
+    async (listData) => {
 
-        console.log("LIST DATA:", listData); 
-        if (listData.type == 'profile') {
-          
-          let profile = listData.profileArray[0];
+      console.log("LIST DATA:", listData); 
+      if (listData.type == 'profile') {
+        
+        let profile = listData.profileArray[0];
 
-          // Logs
-          if (charadex.tools.checkArray(profile.characterlog)) {
-            try {
-              logs = await charadex.initialize.page(
-                profile.characterlog,
-                charadex.page.masterlist.relatedData['character log']
-              );
-            } catch(err) {
-              console.error('Failed to get the related logs.', err);
-            } finally {
-              if (logs) {
-                console.log('Initialized related logs!', logs);
-              }
-            }
-          }
-
-          // Set the player url
-          let pageUrl = charadex.url.getPageUrl(charadex.page.inventory.sitePage);
-          $('.playerlink').attr('href', charadex.url.addUrlParameters(pageUrl, { profile: profile.player }));
-
+        // Logs
+        if (charadex.tools.checkArray(profile.characterlog)) {
+          let logs = await charadex.initialize.page(
+            profile.characterlog,
+            charadex.page.masterlist.relatedData['character log']
+          );
+          console.log('Initialized related logs!');
         }
-    });
-  } catch(err) {
-    console.error('Failed to initialize the page.', err);
-  } finally {
-    if (dex) {
-      console.log('Initialized the page!', dex);
-    }
-    charadex.tools.loadPage('.softload', 500);
-  }
 
+        // Set the player url
+        let pageUrl = charadex.url.getPageUrl(charadex.page.inventory.sitePage);
+        $('.playerlink').attr('href', charadex.url.addUrlParameters(pageUrl, { profile: profile.player }));
+
+      }
+  });
+
+  charadex.tools.loadPage('.softload', 500);
 });
