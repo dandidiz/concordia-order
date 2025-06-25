d62deb4228cc8cfe567db045b144dbc1b5a1bc31
Juni <junikimdraws@gmail.com>
2025-06-24 19:27:22 -0500

    fix promises on characters

--- styles/js/pages/characters.js
+++ styles/js/pages/characters.js
@@ -8,36 +8,43 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-  let dex = await charadex.initialize.page(
-    null,
-    charadex.page.masterlist,
-    null,
-    async (listData) => {
+  try {
+    let dex = await charadex.initialize.page(
+      null,
+      charadex.page.masterlist,
+      null,
+      async (listData) => {
 
-      console.log("LIST DATA:", listData); 
-      if (listData.type == 'profile') {
-        
-        let profile = listData.profileArray[0];
+        console.log("LIST DATA:", listData); 
+        if (listData.type == 'profile') {
+          
+          let profile = listData.profileArray[0];
 
-        // Logs
-        if (charadex.tools.checkArray(profile.characterlog)) {
-          let logs = await charadex.initialize.page(
-            profile.characterlog,
-            charadex.page.masterlist.relatedData['character log']
-          ).then( () => {
-            console.log('Initialized related logs!', logs);
-          });
+          // Logs
+          if (charadex.tools.checkArray(profile.characterlog)) {
+            try {
+              let logs = await charadex.initialize.page(
+                profile.characterlog,
+                charadex.page.masterlist.relatedData['character log']
+              );
+            } catch(err) {
+              console.error('Failed to get the related logs.', err);
+            } finally {
+              console.log('Initialized related logs!', logs);
+            }
+          }
+
+          // Set the player url
+          let pageUrl = charadex.url.getPageUrl(charadex.page.inventory.sitePage);
+          $('.playerlink').attr('href', charadex.url.addUrlParameters(pageUrl, { profile: profile.player }));
+
         }
-
-        // Set the player url
-        let pageUrl = charadex.url.getPageUrl(charadex.page.inventory.sitePage);
-        $('.playerlink').attr('href', charadex.url.addUrlParameters(pageUrl, { profile: profile.player }));
-
-      }
-
-    }
-  ).then( () => {
+    });
+  } catch(err) {
+    console.error('Failed to initialize the page.', err);
+  } finally {
+    console.log('Initialized the page!', dex);
     charadex.tools.loadPage('.softload', 500);
-  });
+  }
 
 });
