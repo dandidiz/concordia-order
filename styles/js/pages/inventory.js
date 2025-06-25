b3399b931844f5bf16cb49fadde89a5872e9290b
Juni <junikimdraws@gmail.com>
2025-06-24 20:03:23 -0500

    aand inventory

--- styles/js/pages/inventory.js
+++ styles/js/pages/inventory.js
@@ -8,57 +8,47 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-  try {
-    let dex = await charadex.initialize.page(
-      null,
-      charadex.page.inventory,
-      null, 
-      async (listData) => {
+  let dex = await charadex.initialize.page(
+    null,
+    charadex.page.inventory,
+    null, 
+    async (listData) => {
 
-        console.log("LIST DATA:", listData); 
-        if (listData.type == 'profile') {
+      console.log("LIST DATA:", listData); 
+      if (listData.type == 'profile') {
 
-          let profile = listData.profileArray[0];
-          
-          // Inventory
-          let fixedData = await charadex.manageData.inventoryFix(profile)
-            .then( () => {
-              charadex.initialize.groupGallery(
-                charadex.page.inventory.inventoryConfig,
-                fixedData,
-                'type',
-                charadex.url.getPageUrl('items')
-              );
-              console.log('Initialized inventory! Fixed data:', fixedData);
-            });
+        let profile = listData.profileArray[0];
+        let fixedData = await charadex.manageData.inventoryFix(profile);
 
-          // Designs
-          if (charadex.tools.checkArray(profile.characters)) {
-            let designs = await charadex.initialize.page(
-              profile.characters,
-              charadex.page.inventory.relatedData['characters'],
-            ).then( () => {
-              console.log('Initialized related characters!', designs);
-            });
-          }
+        // Inventory
+        charadex.initialize.groupGallery(
+          charadex.page.inventory.inventoryConfig,
+          fixedData,
+          'type',
+          charadex.url.getPageUrl('items')
+        );
+        console.log('Initialized inventory gallery!');
 
-          // Logs
-          if (charadex.tools.checkArray(profile.inventorylog)) {
-            let logs = await charadex.initialize.page(
-              profile.inventorylog,
-              charadex.page.inventory.relatedData['inventory log'],
-            ).then( () => {
-              console.log('Initialized related logs!', logs);
-            });
-          }
-
+        // Designs
+        if (charadex.tools.checkArray(profile.characters)) {
+          let designs = await charadex.initialize.page(
+            profile.characters,
+            charadex.page.inventory.relatedData['characters'],
+          );
+          console.log('Initialized related characters!');
         }
+
+        // Logs
+        if (charadex.tools.checkArray(profile.inventorylog)) {
+          let logs = await charadex.initialize.page(
+            profile.inventorylog,
+            charadex.page.inventory.relatedData['inventory log'],
+          );
+          console.log('Initialized related logs!');
+        }
+
+      }
     });
-  } catch(err) {
-    console.log('Failed to initialize the page.', err);
-  } finally {
-    console.log('Initialized the page!', dex);
-    charadex.tools.loadPage('.softload', 500);
-  }
-  
+    
+  charadex.tools.loadPage('.softload', 500);  
 });
