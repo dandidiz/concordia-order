a9da43dfd6eab0d7671eb00964af70a31adbc30e
Juni <junikimdraws@gmail.com>
2025-07-16 22:39:01 -0500

    try adding relationships

--- styles/js/pages/characters.js
+++ styles/js/pages/characters.js
@@ -36,6 +36,29 @@ document.addEventListener("DOMContentLoaded", async () => {
         let pageUrl = charadex.url.getPageUrl(charadex.page.inventory.sitePage);
         $('.playerlink').attr('href', charadex.url.addUrlParameters(pageUrl, { profile: profile.player }));
 
+        // Oh lordt, it's rels time
+        if (profile.relationships && profile.relationships === 'string') {
+
+          // our rels column has a textjoin of all relationships
+          // we need to put it back into array form
+          let relSplit = profile.relationships.split(';;;');
+          // number of columns per row we anticipate
+          const numCols = 5;
+
+          for (let i = 0; i < relSplit.length; i += numCols) {
+            // get the relationship
+            let chunk = array.slice(i, i + numCols);
+            // Create the DOM elements
+            let relElement = `<div class="row">`
+            for (let j = 0; j < chunk.length; j++) {
+              relElement += `<div class="col">${chunk[j]}</div>`
+            }
+            relElement += `</div>`
+
+            $('#rel-container').html(relElement);
+          }
+        }
+
       }
   });
 
